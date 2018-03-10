c626624ff4217ba4370e481450b4059e8f6177a3
fajri <fajri21@gmail.com>
2018-03-10 22:37:07 +0800

    yo

--- tongsampah.sql
+++ tongsampah.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 10, 2018 at 08:17 PM
--- Server version: 5.7.17-0ubuntu0.16.04.2
--- PHP Version: 7.1.10-1+ubuntu16.04.1+deb.sury.org+1
+-- Generation Time: Mar 10, 2018 at 02:33 PM
+-- Server version: 5.6.20-log
+-- PHP Version: 7.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `tongsampah`
@@ -26,22 +26,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `data_sampah`
 --
 
-CREATE TABLE `data_sampah` (
-  `id_sampah` int(10) NOT NULL,
-  `id_kat` int(10) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `data_sampah` (
+`id_sampah` int(10) NOT NULL,
   `id_user` int(10) NOT NULL,
+  `id_kat` int(10) DEFAULT NULL,
   `ket` text,
-  `etiket` text
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `etiket` text,
+  `berat` int(11) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `data_sampah`
 --
 
-INSERT INTO `data_sampah` (`id_sampah`, `id_kat`, `id_user`, `ket`, `etiket`) VALUES
-(8, 2, 5, 'adasdasdasdasd', 'asdsadasdasda'),
-(7, 2, 5, 'adasdasdasdasd', 'asdsadasdasda'),
-(6, 1, 5, 'asbkasbdkd', 'asdsakdbaksj');
+INSERT INTO `data_sampah` (`id_sampah`, `id_user`, `id_kat`, `ket`, `etiket`, `berat`, `status`) VALUES
+(14, 2, 1, 'aasdasd', '03001', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -49,11 +49,11 @@ INSERT INTO `data_sampah` (`id_sampah`, `id_kat`, `id_user`, `ket`, `etiket`) VA
 -- Table structure for table `kategori`
 --
 
-CREATE TABLE `kategori` (
-  `id_kat` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `kategori` (
+`id_kat` int(10) NOT NULL,
   `nama_kat` varchar(100) DEFAULT NULL,
   `harga_kat` int(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `kategori`
@@ -70,12 +70,12 @@ INSERT INTO `kategori` (`id_kat`, `nama_kat`, `harga_kat`) VALUES
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id_user` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+`id_user` int(10) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(50) NOT NULL,
   `level` int(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `user`
@@ -95,20 +95,19 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `level`) VALUES
 -- Indexes for table `data_sampah`
 --
 ALTER TABLE `data_sampah`
-  ADD PRIMARY KEY (`id_sampah`),
-  ADD KEY `id_kat` (`id_kat`);
+ ADD PRIMARY KEY (`id_sampah`), ADD KEY `id_kat` (`id_kat`), ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
-  ADD PRIMARY KEY (`id_kat`);
+ ADD PRIMARY KEY (`id_kat`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id_user`);
+ ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -118,17 +117,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `data_sampah`
 --
 ALTER TABLE `data_sampah`
-  MODIFY `id_sampah` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+MODIFY `id_sampah` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kat` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+MODIFY `id_kat` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
