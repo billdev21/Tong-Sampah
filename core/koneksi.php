6688419e19957825a012dcdc068d9d37bbff0373
Zein <zein.ismalingga@gmail.com>
2018-03-10 12:56:10 +0800

    tambah koneksi.php

---
+++ core/koneksi.php
@@ -0,0 +1,16 @@
+<?php 
+
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database =  "tongsampah";
+
+$link = mysqli_connect($host, $user,$password, $database);
+
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
