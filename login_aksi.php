a20838757a5a5bc9707e73b78e55ac3f837a4ed3
Zein <zein.ismalingga@gmail.com>
2018-03-11 09:33:51 +0800

    fix dompet user

--- login_aksi.php
+++ login_aksi.php
@@ -44,15 +44,10 @@ elseif($st==2){
     header('Location: '. BASE_URL. "/user");
 }
 elseif($st==3){
-<<<<<<< HEAD
-    $_SESSION["id"]=$id_user;
-    $_SESSION["username"]=$row['username'];
-=======
     
     $_SESSION["id"]=$id_user;
     $_SESSION["username"]=$row['username'];
     $_SESSION["level"]=$row['level'];
->>>>>>> 707470989a06fce482ce510ec6f7fbbf4fd0e6b2
     header('Location: '. BASE_URL. "/driver");
 }
 else{
